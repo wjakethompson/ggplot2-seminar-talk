6e5cba6e0ddc7cd9c2ba3e54da7152f3ef4a828e
Jake Thompson <hawklytics@gmail.com>
2016-10-05 12:13:44 -0500

    added notebook interface

--- render.R
+++
@@ -1,5 +0,0 @@
-rmarkdown::render("presentation.Rmd", output_format = "html_document",
-  output_file = "index.html")
-
-rmarkdown::render("presentation.Rmd", output_format = "github_document",
-  output_file = "README.md")
